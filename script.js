efd8d5711cdc4e9b720bb7c14721d44132c41186
tupa <jcapanegra@gmail.com>
2023-07-31 20:04:21 -0300

    preeentrega 3

--- script.js
+++ script.js
@@ -1,16 +1,67 @@
-const stickers = [
-  { nombre: "Sticker 1", precioUnitario: 5, cantidad: 0 },
-  { nombre: "Sticker 2", precioUnitario: 3, cantidad: 0 },
-  { nombre: "Sticker 3", precioUnitario: 7, cantidad: 0 }
-];
+class Sticker {
+  constructor(nombre, precioUnitario, cantidad) {
+    this.nombre = nombre;
+    this.precioUnitario = precioUnitario;
+    this.cantidad = cantidad;
+  }
+}
+
+// Obtener los datos de stickers desde el Storage (localStorage o sessionStorage)
+let storedStickers = localStorage.getItem("stickers");
+let stickers = [];
+
+if (storedStickers) {
+  stickers = JSON.parse(storedStickers);
+} else {
+  // Si no hay datos en el Storage, crear los datos iniciales
+  stickers = [
+    new Sticker("Sticker 1", 5, 0),
+    new Sticker("Sticker 2", 3, 0),
+    new Sticker("Sticker 3", 7, 0)
+  ];
+}
+
+function actualizarStorage() {
+  // Guardar los datos de stickers en el Storage (localStorage o sessionStorage)
+  localStorage.setItem("stickers", JSON.stringify(stickers));
+}
+
+function actualizarDetalleCompra() {
+  let detalleElement = document.getElementById("detalle-compra");
+
+  if (!detalleElement) {
+    // Verificar si el elemento "detalle-compra" existe antes de actualizarlo
+    console.error("Error: El elemento 'detalle-compra' no existe.");
+    return;
+  }
+
+  detalleElement.innerHTML = "";
+
+  stickers.forEach(sticker => {
+    let subtotal = sticker.cantidad * sticker.precioUnitario;
+
+    let stickerDetalle = document.createElement("p");
+    stickerDetalle.textContent = `${sticker.nombre}: Cantidad: ${sticker.cantidad}, Subtotal: $${subtotal}`;
+    
+    detalleElement.appendChild(stickerDetalle);
+  });
+}
 
 function calcularTotal() {
-  let cantidadInputs = document.getElementsByClassName("cantidad-input");
   let total = 0;
   let isValidInput = true;
 
-  for (let i = 0; i < cantidadInputs.length; i++) {
-    let cantidadValue = cantidadInputs[i].value.trim();
+  for (let i = 0; i < stickers.length; i++) {
+    let cantidadInput = document.getElementById(`cantidad${i + 1}`);
+
+    if (!cantidadInput) {
+      // Verificar si el input de cantidad existe antes de acceder a él
+      console.error(`Error: El input de cantidad con ID 'cantidad${i + 1}' no existe.`);
+      isValidInput = false;
+      break;
+    }
+
+    let cantidadValue = cantidadInput.value.trim();
 
     if (!cantidadValue || isNaN(cantidadValue) || parseInt(cantidadValue) < 0) {
       isValidInput = false;
@@ -32,11 +83,26 @@ function calcularTotal() {
   if (isValidInput) {
     let resultadoElement = document.getElementById("total");
     resultadoElement.textContent = "Total a pagar: $" + total;
+
+    // Actualizar el detalle de la compra
+    actualizarDetalleCompra();
+
+    // Actualizar el Storage con los datos de stickers
+    actualizarStorage();
   } else {
-    alert("Por favor, ingrese valores numéricos positivos en los tres campos de cantidad.");
+    alert("Por favor, ingrese valores numéricos positivos en los campos de cantidad.");
   }
 
   if (isNaN(total)) {
     alert("El total a pagar es inválido. Por favor, ingrese números válidos en todos los campos de cantidad.");
   }
+}
+
+// Capturar evento click en el botón
+document.getElementById("calcularButton").addEventListener("click", calcularTotal);
+
+// Capturar evento de cambio en los inputs de cantidad
+const cantidadInputs = document.getElementsByClassName("cantidad-input");
+for (let i = 0; i < cantidadInputs.length; i++) {
+  cantidadInputs[i].addEventListener("input", calcularTotal);
 }
